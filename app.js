afe74a6f3a35a87dd406357e32f4c247a19edcca
Max Skorokhodov <skormaksim@mail.ru>
2023-05-09 15:58:22 +0300

    feat: добавить cors

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes/index');
 
 const { PORT, MONGO_URL } = require('./config');
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ['http://localhost:3001', 'https://skor.nomoredomains.monster'],
+  }),
+);
 
 mongoose.connect(MONGO_URL, {});
 
